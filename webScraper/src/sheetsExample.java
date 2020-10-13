5cfc69a54c19e6811e23bef64029b62cfe029b77
Jennifer V <vu.jennifer.chau@gmail.com>
2020-10-12 23:37:33 -0400

    Created application to upload data to google sheets

--- webScraper/src/sheetsExample.java
+++ webScraper/src/sheetsExample.java
@@ -1,19 +1,32 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.api.services.sheets.v4.model.AppendValuesResponse;
 import com.google.api.services.sheets.v4.model.ValueRange;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class sheetsExample {
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS);
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
     public static void main(String args[]) throws IOException, GeneralSecurityException {
         // The ID of the spreadsheet to update.
         String spreadsheetId = "1V1q8fhCVtTkACBJr5W88UKs-MgWwkByXT74dyyDbRt0"; // TODO: Update placeholder value.
@@ -26,12 +39,12 @@ public class sheetsExample {
         String valueInputOption = "RAW"; // TODO: Update placeholder value.
 
         // How the input data should be inserted.
-        String insertDataOption = ""; // TODO: Update placeholder value.
+        String insertDataOption = "INSERT_ROWS"; // TODO: Update placeholder value.
 
         // TODO: Assign values to desired fields of `requestBody`:
         ValueRange requestBody = new ValueRange();
-        List<List<Object>> x = Arrays.asList(Arrays.asList(1,2));
-        requestBody = requestBody.setValues(x);
+        List<List<Object>> x = Arrays.asList(Arrays.asList(1,"www.hello.com"));
+        requestBody.setValues(x);
 
         Sheets sheetsService = createSheetsService();
         Sheets.Spreadsheets.Values.Append request =
@@ -45,6 +58,30 @@ public class sheetsExample {
         System.out.println(response);
     }
 
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final HttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = sheetsExample.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
     public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
         HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
@@ -56,15 +93,14 @@ public class sheetsExample {
         //   "https://www.googleapis.com/auth/drive"
         //   "https://www.googleapis.com/auth/drive.file"
         //   "https://www.googleapis.com/auth/spreadsheets"
-        HttpRequestInitializer credential = new HttpRequestInitializer() {
-            @Override
-            public void initialize(HttpRequest httpRequest) throws IOException {
-
-            }
-        };
+        //HttpRequestInitializer credential = new HttpRequestInitializer("");
+        //SheetsRequestInitializer credential = new SheetsRequestInitializer();
+        Credential credential = getCredentials(httpTransport);
 
         return new Sheets.Builder(httpTransport, jsonFactory, credential)
                 .setApplicationName("Google-SheetsSample/0.1")
                 .build();
     }
+
+
 }
