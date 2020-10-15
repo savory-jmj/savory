b8d37914c41f7d95daed109f282d9ed732d9640a
Jennifer V <vu.jennifer.chau@gmail.com>
2020-10-14 22:10:29 -0400

    Scraped lunch and dinner recipes

--- webScraper/src/GoogleSheetsDatabase.java
+++ webScraper/src/GoogleSheetsDatabase.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 public class GoogleSheetsDatabase {
     public static void main (String[] args) throws IOException, GeneralSecurityException {
+        String breakfastLink = "https://www.allrecipes.com/recipes/78/breakfast-and-brunch/";
+        String lunchLink = "https://www.allrecipes.com/recipes/17561/lunch/";
+        String dinnerLink = "https://www.allrecipes.com/recipes/17562/dinner/";
         GoogleSheetsAppend googleSheetsAppend = new GoogleSheetsAppend("1V1q8fhCVtTkACBJr5W88UKs-MgWwkByXT74dyyDbRt0", "A1", "RAW", "INSERT_ROWS");
-        Webscraper webscraper = new Webscraper("https://www.allrecipes.com/recipes/78/breakfast-and-brunch/", "a.recipeCard__titleLink");
+        Webscraper webscraper = new Webscraper(dinnerLink, "a.recipeCard__titleLink");
         List<List<Object>> list = webscraper.visitPage();
         googleSheetsAppend.appendRow(list);
     }
